1d6d91a17183577f412ac2fa37c101102b5459d8
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 21:41:16 +0530

    Implicit Type conversion in java.

---
+++ src/ImplicitTypeConversion.java
@@ -0,0 +1,9 @@
+public class ImplicitTypeConversion {
+    public static void main(String[] args) {
+        //Converts smaller data-type into larger data type.
+        //happens automatically.
+        int numInt = 10;
+        double numDouble = numInt;
+        System.out.println(numDouble);
+    }
+}
