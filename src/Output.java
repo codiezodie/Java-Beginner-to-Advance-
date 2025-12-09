ac99fb20a6f55f41d0a17758fc02237aa2f54934
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 15:54:33 +0530

    Using Scanner class to take input and showing this input through System.out.println().

---
+++ src/Output.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("How are you?");
+        Scanner sc = new Scanner(System.in);
+        //System.out.println(sc);
+        System.out.println(sc.nextLine());
+
+        //Scanner class is in java.util package
+
+    }
+}
