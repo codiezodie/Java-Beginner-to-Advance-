3f3c7e4479dd332e60e0bd68564310089ead66eb
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 15:53:08 +0530

    Starting java from beginning to advance with small code.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //System is a class, out is a public static object of the PrintStream class inside System,
+        // and print() is a method from the PrintStream class.
+        //print -> Print in Same line
+        //println -> Print in Next Line
+    }
+}
