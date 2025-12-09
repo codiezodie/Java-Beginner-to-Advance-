62aa080fcd9f63ca9baa890270ddc583122c1c1b
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 21:36:44 +0530

    Literals and Identifiers in java.

---
+++ src/Literal_Identifiers.java
@@ -0,0 +1,12 @@
+public class Literal_Identifiers {
+    public static void main(String[] args) {
+
+        int age = 20;             // age is identifier, 20 is literal
+        String name = "Zainab";   // name is identifier, "Zainab" is literal
+        boolean isStudent = true; // isStudent is identifier, true is literal
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(isStudent);
+    }
+}
