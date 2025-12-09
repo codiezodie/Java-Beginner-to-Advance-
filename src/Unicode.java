9e6ac3c72c12471435c37ce5b9eec728e656f7bf
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 21:35:23 +0530

    java follow unicode.

---
+++ src/Unicode.java
@@ -0,0 +1,10 @@
+public class Unicode {
+    public static void main(String[] args) {
+        // Java follows Unicode Ascii value.
+        int a = 'A';
+        System.out.println(a);
+
+        int b = 'a';
+        System.out.println(b);
+    }
+}
