123972d517a28802eb43574cdcdbaf18c90d95e3
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 21:41:41 +0530

    Explicit Type conversion in java.

---
+++ src/ExplicitTypeConversion.java
@@ -0,0 +1,10 @@
+public class ExplicitTypeConversion {
+    public static void main(String[] args) {
+        //Explicit Type Conversion
+        //Converts large data-type into small data-type.
+        //performs manually.
+        double db = 10.75;
+        int num = (int)db;
+        System.out.println(num);
+    }
+}
