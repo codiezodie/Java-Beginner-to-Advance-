78381bf816075685aa6d08e3ef1b55e6f7fcb099
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 21:43:05 +0530

    Simple if-else syntax code.

---
+++ src/if_else.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class if_else {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("hiiiiiii");
+        }
+    }
+}
